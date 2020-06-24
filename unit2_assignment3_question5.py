5f0ff86eb605bcb1da98ccd13a07b5851be60d98
Vinayraju7045 <67084004+Vinayraju7045@users.noreply.github.com>
2020-06-24 11:21:26 +0530

    Add files via upload

---
+++ unit2_assignment3_question5.py
@@ -0,0 +1,74 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 6,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "sin 60 :  -0.3048106211022167\n",
+      "cos pi :  -1.0\n",
+      "tan 90 :  -1.995200412208242\n",
+      "angle of sin(0.8660254037844386) : 43.64563193711739\n",
+      "square root of 400 :  20.0\n",
+      "exponential of 5 : 148.4131591025766\n",
+      "log(1024) with base 2 : 10.0\n",
+      "log(1024) with base 10 :  3.010299956639812\n",
+      "floor and ceiling value of 23.56 :  23 24\n"
+     ]
+    }
+   ],
+   "source": [
+    "import math\n",
+    "print('sin 60 : ',math.sin((60)))\n",
+    "print('cos pi : ',math.cos(math.pi))\n",
+    "print('tan 90 : ',math.tan(90))\n",
+    "x=math.sin(0.8660254037844386)\n",
+    "print('angle of sin(0.8660254037844386) :',math.degrees(x))\n",
+    "print('square root of 400 : ',math.sqrt(400))\n",
+    "print('exponential of 5 :',math.exp(5))\n",
+    "print('log(1024) with base 2 :',math.log2(1024))\n",
+    "print('log(1024) with base 10 : ',math.log10(1024))\n",
+    "print('floor and ceiling value of 23.56 : ',math.floor(23.56),math.ceil(23.56))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
