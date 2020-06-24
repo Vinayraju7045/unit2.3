b3a6ed6b41f1f4ada7fa17e753b098b3af0292ad
Vinayraju7045 <67084004+Vinayraju7045@users.noreply.github.com>
2020-06-24 11:28:14 +0530

    Add files via upload

---
+++ unit2_assignment3_shufflelist.py
@@ -0,0 +1,54 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "The original list is : [100, 1, 2, 3, 30, 40, 'hi', 'hello']\n",
+      "The shuffled list is : [3, 2, 30, 40, 1, 'hello', 100, 'hi']\n"
+     ]
+    }
+   ],
+   "source": [
+    "import random \n",
+    "l1 = [100,1,2,3,30,40,'hi','hello'] \n",
+    "print (\"The original list is : \" + str(l1))  \n",
+    "random.shuffle(l1) \n",
+    "print (\"The shuffled list is : \" +  str(l1))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
